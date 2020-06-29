aa5b920220e74213d10371f619dfb4f0dc56adfd
KacperMitkowski <43199382+KacperMitkowski@users.noreply.github.com>
2020-06-29 18:50:35 +0200

    Gameboard drawing added

--- Snake_Game/MainWindow.xaml.cs
+++ Snake_Game/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace Snake_Game
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int CELL_WIDTH = 20;
+        private const int CELL_HEIGHT = 20;
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +29,42 @@ namespace Snake_Game
 
         private void Window_ContentRendered(object sender, EventArgs e)
         {
+            DrawGameArea();
+        }
 
+        private void DrawGameArea()
+        {
+            int nextX = 0, nextY = 0, rowCounter = 1;
+            bool xIsOdd = true;
+            bool gameBoardIsDrawing = true;
+
+            while(gameBoardIsDrawing)
+            {
+                Rectangle rect = new Rectangle()
+                {
+                    Width = CELL_WIDTH,
+                    Height = CELL_HEIGHT,
+                    Fill = xIsOdd ? Brushes.White : Brushes.Black
+                };
+                GameArea.Children.Add(rect);
+                Canvas.SetTop(rect, nextY);
+                Canvas.SetLeft(rect, nextX);
+                xIsOdd = !xIsOdd;
+                nextX += CELL_WIDTH;
+
+                if(nextX >= GameArea.ActualWidth)
+                {
+                    nextX = 0;
+                    nextY += CELL_HEIGHT;
+                    rowCounter++;
+                    xIsOdd = (rowCounter % 2 != 0);
+                }
+
+                if(nextY >= GameArea.ActualHeight)
+                {
+                    gameBoardIsDrawing = false;
+                }
+            }
         }
     }
 }
